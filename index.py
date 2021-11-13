d9a4f1677aa3583a94998aa7c213ed091af93ab6
Romeo <kilazlove@gmail.com>
2021-11-13 22:22:19 +0800

    put inside the function the input process and solve process

--- index.py
+++ index.py
@@ -2,19 +2,25 @@
 # Redo the programs on assignment2 but now with functions that return multiple values 
 # (move all user inputs in one function)
 
-
-apples_quantity = int(input("How many apples you want to buy: "))
-orange_quantity = int(input("How many oranges you want to buy: "))
-
 apple_Price = 20
 orange_Price = 25
 
 apple_Amount = 0
 orange_Amount = 0
 
-if apples_quantity < 0 or orange_quantity < 0:
-    print("Quantity cannot be less than zero")
-else :
-    apple_Amount = apples_quantity * apple_Price
-    orange_Amount = orange_quantity * orange_Price
-    print("The total amount is: " + str(apple_Amount + orange_Amount))
+def getInputs():
+    global apples_quantity
+    global orange_quantity
+
+    apples_quantity = int(input("How many apples you want to buy: "))
+    orange_quantity = int(input("How many oranges you want to buy: "))
+
+    return apples_quantity, orange_quantity
+
+def solve():
+    if apples_quantity < 0 or orange_quantity < 0:
+        print("Quantity cannot be less than zero")
+    else :
+        apple_Amount = apples_quantity * apple_Price
+        orange_Amount = orange_quantity * orange_Price
+        print("The total amount is: " + str(apple_Amount + orange_Amount))
